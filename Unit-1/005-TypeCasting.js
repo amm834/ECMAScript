cea1b3dea2927dbc418ece44afe91a0004447f9f
Aung Myat Moe <aungmyatmoe.edu@gmail.com>
2020-12-16 23:41:13 +0630

    Unit-1|005-Type Casting

---
+++ Unit-1/005-TypeCasting.js
@@ -0,0 +1,9 @@
+let x = "20"; //String
+let num = Number(20);//Number
+let toString = true.toString();//String "true"
+let fool = "200 abc 900";//String
+let nan = Number(fool);//NaN -> Not a Number
+let toDay = Date();
+let toDayObj = new Date();
+let toDayTime = toDayObj.getTime();
+console.log(toDayTime);
