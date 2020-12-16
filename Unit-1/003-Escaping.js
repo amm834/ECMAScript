ab0208dd0dfd3a1b2028cff4d2cffc8669798b6f
Aung Myat Moe <aungmyatmoe.edu@gmail.com>
2020-12-16 23:27:28 +0630

    Unit-1|004-Escaping Charcters

---
+++ Unit-1/003-Escaping.js
@@ -0,0 +1,8 @@
+let singleQuoteEscape = 'I\'m from Letpadan';
+let doubleQuoteEscape = "My name is \"Aung Myat Moe\".";
+
+// Should Use Instead Of Above Code
+let bio = "I'm from Letpadan";
+let nameIntro = 'My name is "Aung Myat Moe"';
+console.log(singleQuoteEscape);
+console.log(doubleQuoteEscape);
