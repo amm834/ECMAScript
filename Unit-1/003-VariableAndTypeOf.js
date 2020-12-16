dc534eb2b0f6a74b3271f7cb695cc7ad0dd7df67
Aung Myat Moe <aungmyatmoe.edu@gmail.com>
2020-12-16 23:21:01 +0630

    Unit-1|003-VariableAndTypeOf JS

---
+++ Unit-1/003-VariableAndTypeOf.js
@@ -0,0 +1,16 @@
+let firstName = "Aung";
+let middleName = "Myat";
+let lastName = "Moe";
+let fullName = firstName + middleName + lastName;
+console.log(fullName);//AungMyatMoe
+console.log(typeof(fullName));//String
+// Operation
+let x = 10;
+let y = 7;
+let z = x + y; // +,-,*,/,%
+console.log(z);//17
+console.log(3**3);//27 (3 power of 3) = 3×3×3 = 27 (Exponetial)
+console.log(null);//null Object
+let name;
+console.log(name);//undefined
+console.log(typeof(name));//undefined
