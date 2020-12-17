a5561d50bcfab5c8901dde45443ff4020d6849b7
Aung Myat Moe <aungmyatmoe.edu@gmail.com>
2020-12-17 10:07:58 +0630

    Unit-1|007-Comparasion

---
+++ Unit-1/009-Comparasion.js
@@ -0,0 +1,9 @@
+let x=10,y=5;
+console.log("Equal?",(x==y));
+console.log("Not Equal?",(x!=y));
+console.log("Identical?",(x===y));
+console.log("Not Identical?",(x !==y));
+console.log("Greater Than?",(x >y));
+console.log("Less Than?",(x < y));
+console.log("Greater Than or Equal?",(x  >= y));
+console.log("Less Than or Equal?",(x <= y));
