fe236c3c9e1b99b21428688c0c5afbf5e7bac88f
Aung Myat Moe <aungmyatmoe.edu@gmail.com>
2020-12-17 10:01:09 +0630

    Unit-1|006-Arithmic Operator

---
+++ Unit-1/006-Arithmetic Operators.js
@@ -0,0 +1,9 @@
+let x,y;
+x = 5,y=10;
+console.log("Addition",x+y);
+console.log("Subtraction",y-x);
+console.log("Multiplication",x*y);
+console.log("Division",y/x);
+console.log("Modulus",y%x);
+console.log("Auto Increment",x++);
+console.log("Auto Decrement",--x);
