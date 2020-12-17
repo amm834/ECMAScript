7757a6e4f06be6a6ebfbd2fee9bc0f1b12c99094
Aung Myat Moe <aungmyatmoe.edu@gmail.com>
2020-12-17 10:12:38 +0630

    Unit-1|008-Logical Operator

---
+++ Unit-1/008-Logical Operator.js
@@ -0,0 +1,10 @@
+// OR Operator
+console.log(true||true);
+console.log(true||false);
+console.log(false||false);
+console.log(false||true);
+//AND Operator
+console.log(true && true);
+console.log(true && false);
+console.log(false && false);
+console.log(false && true);
