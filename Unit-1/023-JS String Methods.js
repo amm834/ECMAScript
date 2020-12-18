6e917bd5a90e7e0e02a93e579d7b990160187dec
Aung Myat Moe <aungmyatmoe.edu@gmail.com>
2020-12-18 23:36:40 +0630

    024-String Methods

---
+++ Unit-1/023-JS String Methods.js
@@ -0,0 +1,7 @@
+let devName = "Aung Myat Moe";
+let nameLength = devName.length;
+console.log(nameLength);
+let indexOfAung = devName.indexOf('Myat');
+console.log(indexOfAung);
+let strObj = new String("Hello Word!");
+console.log(strObj);
