e4df7c551ac53c87952af542a89dab468e148fcb
Aung Myat Moe <aungmyatmoe.edu@gmail.com>
2020-12-18 23:39:47 +0630

    023-Numver Methods

---
+++ Unit-1/024-Number Methods.js
@@ -0,0 +1,9 @@
+let priNum = "20.001";
+let num = Number(priNum);
+console.log(num);
+let parNum = parseInt(priNum);
+console.log(parNum);
+let parFloat = parseFloat(priNum);
+console.log(parFloat);
+let fixed = num.toFixed();
+console.log(fixed);  
