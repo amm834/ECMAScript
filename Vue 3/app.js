7c5aae066a46c15976c985c925a33f72671980ae
amm834 <aungmyatmoe.edu@gmail.com>
2020-12-22 20:57:05 +0630

    Project 01 : Setup

---
+++ Vue 3/app.js
@@ -0,0 +1,32 @@
+Vue.createApp({
+ data() {
+  return {
+   cards: [
+    {
+    title: 'Vue',
+    link: {
+     url: 'vuejs.org',
+     image: 'vue.png'
+    },
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni ratione molestiae aperiam autem beatae unde vel. Libero consectetur consequatur iste perferendis, ea numquam, distinctio fuga, quae vitae eligendi debitis vel!'
+   },
+    {
+    title: 'React',
+    link: {
+     url: 'reactjs.org',
+     image: 'react.png'
+    },
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni ratione molestiae aperiam autem beatae unde vel. Libero consectetur consequatur iste perferendis, ea numquam, distinctio fuga, quae vitae eligendi debitis vel!'
+   },
+    {
+    title: 'Angular',
+    link: {
+     url: 'amgular.io',
+     image: 'angular.png'
+    },
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni ratione molestiae aperiam autem beatae unde vel. Libero consectetur consequatur iste perferendis, ea numquam, distinctio fuga, quae vitae eligendi debitis vel!'
+   },
+   ]
+  }
+ }
+})
