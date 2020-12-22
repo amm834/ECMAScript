1212307cfc3d85c5259437ea387f54217691ebdb
amm834 <aungmyatmoe.edu@gmail.com>
2020-12-22 21:39:50 +0630

    Project 01 : Finished Landing Project

--- Vue 3/app.js
+++ Vue 3/app.js
@@ -6,7 +6,7 @@ const app = Vue.createApp({
     title: 'Vue',
     link: {
      url: 'vuejs.org',
-     image: 'vue.png'
+     image: 'https://vuejs.org/images/logo.png'
     },
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni ratione molestiae aperiam autem beatae unde vel. Libero consectetur consequatur iste perferendis, ea numquam, distinctio fuga, quae vitae eligendi debitis vel!'
    },
@@ -14,7 +14,7 @@ const app = Vue.createApp({
     title: 'React',
     link: {
      url: 'reactjs.org',
-     image: 'react.png'
+     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png'
     },
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni ratione molestiae aperiam autem beatae unde vel. Libero consectetur consequatur iste perferendis, ea numquam, distinctio fuga, quae vitae eligendi debitis vel!'
    },
@@ -22,7 +22,7 @@ const app = Vue.createApp({
     title: 'Angular',
     link: {
      url: 'amgular.io',
-     image: 'angular.png'
+     image: 'https://angular.io/assets/images/logos/angular/angular.svg'
     },
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni ratione molestiae aperiam autem beatae unde vel. Libero consectetur consequatur iste perferendis, ea numquam, distinctio fuga, quae vitae eligendi debitis vel!'
    },
@@ -30,4 +30,21 @@ const app = Vue.createApp({
   };
  }
 });
+app.component('cards-component',{
+ props:['title','link','text'],
+ template:`
+ <div class="card mt-3" style="width: 18rem;">
+  <img :src="link.image" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">
+    {{title}}
+    </h5>
+    <p class="card-text">
+    {{text}}
+    </p>
+    <a :href="link.url" class="btn btn-primary">Go To {{title}}</a>
+  </div>
+</div>
+ `
+})
 app.mount('#app');
